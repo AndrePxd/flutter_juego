e664d6200ea85334ee1f60ceb638c52973ee74a4
AndrePxd <andrepanique@gmail.com>
2023-05-19 00:48:35 -0400

    Change level 2

--- lib/nivel2.dart
+++ lib/nivel2.dart
@@ -1,25 +1,31 @@
 import 'package:flutter/material.dart';
 
+class WordItem {
+  String word;
+  bool isDragged;
+  WordItem(this.word, {this.isDragged = false});
+}
+
 class DragAndDropScreen extends StatefulWidget {
   @override
   _DragAndDropScreenState createState() => _DragAndDropScreenState();
 }
 
 class _DragAndDropScreenState extends State<DragAndDropScreen> {
-  List<String> words = [
-    'Hola 1',
-    'Hola 2',
-    'Hola 3',
-    'Hola 4',
-    'Hola 5',
-    'Hola 6',
-    'Hola 7',
-    'Hola 8',
-    'Hola 9',
-    'Hola 10',
+  List<WordItem> words = [
+    WordItem('Hola 1'),
+    WordItem('Hola 2'),
+    WordItem('Hola 3'),
+    WordItem('Hola 4'),
+    WordItem('Hola 5'),
+    WordItem('Hola 6'),
+    WordItem('Hola 7'),
+    WordItem('Hola 8'),
+    WordItem('Hola 9'),
+    WordItem('Hola 10'),
   ];
 
-  List<String> droppedWords = []; // Lista para almacenar las palabras soltadas
+  List<WordItem> droppedWords = [];
 
   @override
   Widget build(BuildContext context) {
@@ -44,17 +50,19 @@ class _DragAndDropScreenState extends State<DragAndDropScreen> {
                 scrollDirection: Axis.horizontal,
                 itemCount: words.length,
                 itemBuilder: (context, index) {
-                  return Draggable<String>(
+                  return Draggable<WordItem>(
                     data: words[index],
                     child: Container(
                       margin: EdgeInsets.all(8),
                       padding: EdgeInsets.all(12),
                       decoration: BoxDecoration(
-                        color: Colors.brown.shade800,
+                        color: words[index].isDragged
+                            ? Colors.red.shade800
+                            : Colors.green.shade800,
                         borderRadius: BorderRadius.circular(20),
                       ),
                       child: Text(
-                        words[index],
+                        words[index].word,
                         style: TextStyle(fontSize: 16, color: Colors.white),
                       ),
                     ),
@@ -74,11 +82,12 @@ class _DragAndDropScreenState extends State<DragAndDropScreen> {
                         ],
                       ),
                       child: Text(
-                        words[index],
+                        words[index].word,
                         style: TextStyle(fontSize: 16),
                       ),
                     ),
                     childWhenDragging: Container(),
+                    maxSimultaneousDrags: words[index].isDragged ? 0 : 1,
                   );
                 },
               ),
@@ -86,7 +95,7 @@ class _DragAndDropScreenState extends State<DragAndDropScreen> {
           ),
           Align(
             alignment: Alignment.center,
-            child: DragTarget<String>(
+            child: DragTarget<WordItem>(
               builder: (context, candidateData, rejectedData) {
                 return Container(
                   width: 300,
@@ -95,18 +104,62 @@ class _DragAndDropScreenState extends State<DragAndDropScreen> {
                     color: Colors.transparent,
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Center(
-                    child: Text(
-                      droppedWords.join(' '), // Mostrar las palabras soltadas
-                      style: TextStyle(fontSize: 16),
-                    ),
+                  child: Wrap(
+                    spacing: 8.0,
+                    runSpacing: 4.0,
+                    children: droppedWords.map((wordItem) {
+                      return Draggable<WordItem>(
+                        data: wordItem,
+                        child: Container(
+                          margin: EdgeInsets.all(8),
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.red.shade800,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Text(
+                            wordItem.word,
+                            style: TextStyle(fontSize: 16, color: Colors.white),
+                          ),
+                        ),
+                        feedback: Container(
+                          margin: EdgeInsets.all(8),
+                          padding: EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Text(
+                            wordItem.word,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                        childWhenDragging: Container(),
+                        onDragEnd: (details) {
+                          if (!details.wasAccepted) {
+                            setState(() {
+                              wordItem.isDragged = false;
+                              droppedWords.remove(wordItem);
+                            });
+                          }
+                        },
+                      );
+                    }).toList(),
                   ),
                 );
               },
-              onAccept: (word) {
+              onAccept: (wordItem) {
                 setState(() {
-                  droppedWords
-                      .add(word); // Agregar la palabra soltada a la lista
+                  wordItem.isDragged = true;
+                  droppedWords.add(wordItem);
                 });
               },
             ),
